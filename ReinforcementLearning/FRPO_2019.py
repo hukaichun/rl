4f896d5e3f219fbc3357b20e46cb73e8381a1f51
hukaichun <hu.kaichun@gamil.com>
2019-08-20 02:58:29 +0800

    update

--- ReinforcementLearning/FRPO_2019.py
+++ ReinforcementLearning/FRPO_2019.py
@@ -1,142 +1,91 @@
 import tensorflow as tf
 import tensorflow_probability as tfp
-from .util import IMPALA
 import numpy as np
 
 obs_dim = (18,)
 act_dim = (4,)
 
 
+class Controller(tf.keras.Model):
+    def __init__(self, 
+                 featureDims = [32,32],
+                 name = "Controller"):
+        super().__init__(name = name)
+        self._featureMaps = [tf.keras.layers.Dense(unit, tf.nn.relu) for unit in featureDims]
+        self._action_layer = tf.keras.layers.Dense(4, tf.nn.softsign)
+
+    def call(self, state):
+        feature = state
+        for layer in self._featureMaps:
+            feature = layer(feature)
+        return self._action_layer(feature)
+
+
+
+class Critic(tf.keras.Model):
+    def __init__(self,
+                 featureDims = [128,128],
+                 name="critic"):
+        super().__init__(name=name)
+        self._featureMaps = [tf.keras.layers.Dense(unit, tf.nn.relu) for unit in featureDims]
+        self._value_layer = tf.keras.layers.Dense(1)
+
+    def call(self, state):
+        feature = state
+        for layer in self._featureMaps:
+            feature = layer(feature)
+        return self._value_layer(feature)
+
 
 
 class Actor(tf.Module):
-    def __init__(self, 
-                 name="Actor"):
-        super().__init__(name=name)
-        (self.controller,
-         self.exp_rate,
-         self.pdf,
-         self.act) = Actor.build_everything()
+    def __init__(self, name="Actor"):
+        self._controller = Controller()
+        self._sigma = tf.Variable(0.3, name="sigma")
+        self._opt = tf.keras.optimizers.Nadam(0.001)
 
-        self.w = [v for v in self.controller.variables if "kernel" in v.name]
+    def pdf(self, state):
+        mu = self._controller(state)
+        pdf = tfp.distributions.Normal(mu, self._sigma)
+        return pdf
 
-        self.opt = tf.keras.optimizers.Adam(.001)
+    def action(self, state):
+        pdf = self.pdf(state)
+        act = pdf.sample()
+        clipped_act = tf.clip_by_value(act, -1, 1)
+        log_p = pdf.log_prob(clipped_act)
+        marginal_log_p = tf.reduce_mean(log_p, axis=1, keepdims=True)
+        return marginal_log_p, clipped_act
 
-    def regular_exploration(self,obs,
-            low=tf.constant(0.1, dtype=tf.float32),
-            upp=tf.constant(0.5, dtype=tf.float32)):
-        exp = self.exp_rate(obs)
-        too_high = tf.maximum(exp, upp)
-        too_low  = tf.minimum(exp, low)
-        exp_regular = tf.reduce_mean(too_high-too_low)
-        return exp_regular
+    def log_p(self, ref_act, state):
+        pdf = self.pdf(state)
+        log_p = pdf.log_prob(ref_act)
+        return tf.reduce_mean(log_p, axis=1, keepdims=True)
 
-    def regular_kernel_l2(self):
-        ws_l2 = [tf.reduce_sum(x**2) for x in self.w]
-        l2    = tf.reduce_sum(ws_l2)
-        return l2*tf.constant(0.001, dtype=tf.float32)
+    def loss_hinge(self, obs, ref_act, adv, log_mu, margin=tf.constant(.1,dtype=tf.float32)):
+        log_pi = self.log_p(ref_act, obs)
+        surr = (log_pi-log_mu)*tf.sign(adv)
+        hinge = tf.minimum(surr, margin)
+        return tf.reduce_mean(hinge)
 
-    def hinge_loss(self, obs, act, adv, log_mu, epsilon=tf.constant(1., dtype=tf.float32)):
-        with tf.name_scope("RL_hinge"):
-            log_p = self.pdf([obs, act])
-            log_diff = log_p - log_mu
-            surr = tf.sign(adv)*log_diff
-            margin = epsilon
-            averaged_loss = tf.reduce_mean(tf.minimum(surr, margin))
-        return -averaged_loss
+    def loss_regular_l2(self):
+        weight = tf.constant(0.001, dtype=tf.float32)
+        tmp = [tf.reduce_sum(var**2) for var in self._controller.variables]
+        vs2 = tf.reduce_sum(tmp)
+        return weight*vs2
 
-    def train(self, obs, act, adv, log_mu):
+    def update(self, obs, ref_act, adv, log_mu):
         with tf.GradientTape() as tape:
             tape.watch(self.variables)
-            hinge_loss  = self.hinge_loss(obs, act, adv, log_mu)
-            exp_regular = self.regular_exploration(obs)
-            l2_reg      = self.regular_kernel_l2()
-            total_loss  = hinge_loss + exp_regular + l2_reg
-        gradient = tape.gradient(total_loss, self.variables)
+            loss_main = self.loss_hinge(obs, ref_act, adv, log_mu)
+            loss_regu = self.loss_regular_l2()
+            loss_total = loss_main+loss_regu
+        gradient = tape.gradient(loss_total, self.variables)
         clipped_g, norm = tf.clip_by_global_norm(gradient, 1.)
         gvs = zip(clipped_g, self.variables)
         self.opt.apply_gradients(gvs)
+        clipped_sigma = tf.clip_by_value(self._sigma, 0.1, 0.5)
+        self._sigma.assign(clipped_sigma)
+        return loss_main
 
-
-    @staticmethod
-    def build_everything():
-        obs = tf.keras.Input(obs_dim, name="observation")
-        feature = tf.keras.layers.Dense(32, tf.nn.relu)(obs)
-        feature = tf.keras.layers.Dense(32, tf.nn.relu)(feature)
-        action  = tf.keras.layers.Dense(4, tf.nn.softsign)(feature)
-        controller = tf.keras.Model(obs, action, name="Controller")
-
-        variance = tf.keras.layers.Dense(1, tf.nn.softplus)(feature)
-        exploration = tf.keras.Model(obs, variance, name="exploration_rate")
-
-        ref_act = tf.keras.Input(act_dim, name="reference_action")
-        sigma = tf.broadcast_to(variance, tf.shape(action))
-        pdf = tfp.distributions.Normal(action, sigma)
-        log_p = pdf.log_prob(ref_act)
-        marginal_log_p = tf.reduce_mean(log_p, axis=1)
-        policy_pdf = tf.keras.Model([obs, ref_act], marginal_log_p)
-
-        random_sample  = pdf.sample()
-        clipped_sample = tf.clip_by_value(random_sample, -1, 1)
-        marginal_log_p = policy_pdf([obs, clipped_sample])        
-        policy_act = tf.keras.Model(obs, [marginal_log_p, clipped_sample])
-
-        return (controller,
-                exploration,
-                policy_pdf,
-                policy_act)
-
-
-
-class Critic(tf.Module):
-    def __init__(self, gamma, name="Critic"):
-        (self.value, 
-         self.advantage) = Critic.build_everything(gamma)
-
-        self.w = [v for v in self.value.variables if "kernel" in v.name]
-
-        self.opt = tf.keras.optimizers.Adam(.001)
-
-    def regular_kernel_l2(self):
-        ws_l2 = [tf.reduce_sum(x**2) for x in self.w]
-        l2 = tf.reduce_sum(ws_l2)
-        return tf.constant(.001, dtype=tf.float32)*l2
-
-    def mean_squared_error(self, obs, Vpre):
-        predict_error = Vpre - self.value(obs)
-        return tf.reduce_mean(predict_error**2)
-
-    def train(self, obs, Vtar):
-        with tf.GradientTape() as tape:
-            tape.watch(self.variables)
-            error = self.mean_squared_error(obs, Vtar)
-            l2_reg = self.regular_kernel_l2()
-            total_loss = error+l2_reg
-        gradient = tape.gradient(total_loss, self.variables)
-        cliped_g, norm = tf.clip_by_global_norm(gradient)
-        gvs = zip(clipped_g, self.variables)
-        self.opt.apply_gradients(gvs)
-
-    @staticmethod
-    def build_everything(gamma):
-        state = tf.keras.Input(obs_dim, name="state")
-        feature = tf.keras.layers.Dense(128, tf.nn.relu)(state)
-        feature = tf.keras.layers.Dense(128, tf.nn.relu)(feature)
-        value = tf.keras.layers.Dense(1)(feature)
-        state_value = tf.keras.Model(state, value, name="state_value")
-
-        rew        = tf.keras.Input(1, name="reward")
-        next_state = tf.keras.Input(obs_dim, name="next_state")
-        term_flag  = tf.keras.Input(1, name="terminal_flag")
-        low = np.float32(-1./(1-gamma))
-        upp = np.float32(1./(1-gamma))
-        next_value = state_value(next_state)
-        clipped_value = tf.clip_by_value(next_value, low, upp)
-        q_value = rew+tf.constant(gamma,dtype=tf.float32)*(1.-term_flag)*clipped_value
-        print(q_value.shape)
-        adv = q_value-state_value(state)
-        print(adv.shape)
-        advantage = tf.keras.Model([state, rew, term_flag, next_state], adv)
-
-        return state_value, advantage
 
